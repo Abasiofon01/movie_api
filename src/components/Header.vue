809bdd21ba7b0b70d6f8d70cc6b693ca606ec11c
Abasiofon01 <abasiofonudonkang@gmail.com>
2024-11-21 06:16:58 +0100

    feat: imorove header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,59 @@
 <template>
-  <header>
-    <nav class="navbar container">
-      <div class="logo">
-        <h1>Movies</h1>
-      </div>
-      <div class="menu-toggle" @click="toggleMenu">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-      </div>
-      <ul :class="{ active: menuOpen }" class="nav-links">
-        <li v-for="(link, index) in navLinks" :key="index">
-          <a :href="link.route">{{ link.title }}</a>
-        </li>
+  <header class="header" :class="{ active: isSticky }" data-header>
+    <div class="wrapper">
+      <div
+        :class="{ active: isOverlayActive }"
+        class="overlay"
+        data-overlay
+      ></div>
 
-        <li v-if="menuOpen" class="dismiss-btn">
-          <button @click="closeMenu" class="dismiss-btn-icon">X</button>
-        </li>
+      <a href="./index.html" class="logo"> Movies </a>
+
+      <!-- Desktop Navbar -->
+      <nav class="navbar desktop-navbar">
+        <div class="header-actions">
+          <button class="search-btn">
+            <!-- <ion-icon name="search-outline"></ion-icon> -->
+          </button>
+        </div>
+
+        <ul class="desktop-nav-items">
+          <li><a href="./index.html" class="navbar-link">Home</a></li>
+          <li><a href="#" class="navbar-link">Movie</a></li>
+          <li><a href="#" class="navbar-link">Tv Show</a></li>
+          <li><a href="#" class="navbar-link">Web Series</a></li>
+          <li><a href="#" class="navbar-link">Pricing</a></li>
+        </ul>
+      </nav>
+
+      <button class="menu-open-btn" @click="toggleNavbar" data-menu-open-btn>
+        <i class="las la-bars"></i>
+      </button>
+    </div>
+    <!-- Mobile Navbar -->
+    <nav
+      class="navbar mobile-navbar"
+      :class="{ active: isNavbarActive }"
+      data-navbar
+    >
+      <div class="navbar-top">
+        <a href="./" class="logo"> Movies </a>
+
+        <button
+          class="menu-close-btn"
+          @click="toggleNavbar"
+          data-menu-close-btn
+        >
+          <i class="las la-times"></i>
+        </button>
+      </div>
+
+      <ul class="navbar-list">
+        <li><a href="/" class="navbar-link">Home</a></li>
+        <li><a href="#" class="navbar-link">Movie</a></li>
+        <li><a href="#" class="navbar-link">Tv Show</a></li>
+        <li><a href="#" class="navbar-link">Web Series</a></li>
+        <li><a href="#" class="navbar-link">Pricing</a></li>
       </ul>
     </nav>
   </header>
@@ -26,24 +63,26 @@
 export default {
   data() {
     return {
-      menuOpen: false,
-      navLinks: [
-        { title: "Home", route: "#" },
-        { title: "Movie", route: "#" },
-        { title: "Tv Show", route: "#" },
-        { title: "Popular", route: "#" },
-        { title: "Genre", route: "#" },
-        { title: "Upcoming", route: "#" },
-      ],
+      isNavbarActive: false,
+      isOverlayActive: false,
+      isSticky: false,
     };
   },
   methods: {
-    toggleMenu() {
-      this.menuOpen = !this.menuOpen;
+    toggleNavbar() {
+      this.isNavbarActive = !this.isNavbarActive;
+      this.isOverlayActive = !this.isOverlayActive;
+      document.body.classList.toggle("active");
     },
-    closeMenu() {
-      this.menuOpen = false; // Close the menu
+    handleScroll() {
+      this.isSticky = window.scrollY >= 10;
     },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
