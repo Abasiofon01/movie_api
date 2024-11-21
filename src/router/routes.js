419d4de60cd9e9217de46e7179e16cfe97f6389b
Abasiofon01 <abasiofonudonkang@gmail.com>
2024-11-21 22:58:26 +0100

    refactor: remove unused comments

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,3 @@
-// const routes = [
-//   {
-//     path: "/",
-//     component: () => import("layouts/MainLayout.vue"),
-//     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
-//   },
-
-//   // Always leave this as last one,
-//   // but you can also remove it
-//   {
-//     path: "/:catchAll(.*)*",
-//     component: () => import("pages/ErrorNotFound.vue"),
-//   },
-// ];
-
 import Home from "../pages/IndexPage.vue";
 import MovieDetails from "../pages/MovieDetails.vue";
 import SearchPage from "../pages/SearchPage.vue";
@@ -22,12 +7,17 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
-    // children: [{ path: "", component: () => import("../pages/IndexPage.vue") }],
   },
-
-  { path: "/movie/:id", name: "MovieDetails", component: MovieDetails },
-  { path: "/search", name: "SearchResults", component: SearchPage },
-
+  {
+    path: "/movie/:id",
+    name: "MovieDetails",
+    component: MovieDetails,
+  },
+  {
+    path: "/search",
+    name: "SearchResults",
+    component: SearchPage,
+  },
   {
     path: "/:catchAll(.*)*",
     component: () => import("../pages/ErrorNotFound.vue"),
